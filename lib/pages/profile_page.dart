f17ad7ad98e34fc8d908b635c35964d2e8e45b8b
renregidor <renrenregidor@gmail.com>
2025-04-02 00:14:55 +0800

    edit username

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -208,7 +208,7 @@ class _ProfilePageState extends State<ProfilePage> {
                         color: Colors.white),
                     decoration: const InputDecoration(
                       border: InputBorder.none,
-                      hintText: "Enter your name",
+                      hintText: "Enter your username",
                       hintStyle: TextStyle(color: Colors.white70),
                     ),
                     onSubmitted: (value) => _updateUsername(),
